1aa43b304e3214c038a9842ed09ce0dce050fb3a
Bede Carroll <4151340+bedecarroll@users.noreply.github.com>
2025-05-03 22:57:36 -0700

    refactor: Update resume data with personal information and career details

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -1,18 +1,10 @@
-import {
-  ClevertechLogo,
-  ConsultlyLogo,
-  JojoMobileLogo,
-  MonitoLogo,
-  NSNLogo,
-  ParabolLogo,
-} from "@/images/logos";
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
+  name: "Bede Carroll",
+  initials: "BC",
+  location: "San Francisco Bay Area, CA, USA",
+  locationLink: "https://www.google.com/maps/place/San_Francisco_Bay_Area",
   about:
     "Detail-oriented Full Stack Engineer dedicated to building high-quality products.",
   summary: (
@@ -23,219 +15,189 @@ export const RESUME_DATA = {
       leadership.
     </>
   ),
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
+  avatarUrl: "https://avatars.githubusercontent.com/u/4151340?v=4",
+  personalWebsiteUrl: "https://bedecarroll.com",
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: "cv@bedecarroll.com",
+    tel: "+16504958623",
     social: [
       {
         name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
+        url: "https://github.com/bedecarroll",
         icon: GitHubIcon,
       },
       {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
+        url: "https://www.linkedin.com/in/bedecarroll/",
         icon: LinkedInIcon,
       },
       {
         name: "X",
-        url: "https://x.com/BartoszJarocki",
+        url: "https://x.com/bedecarroll",
         icon: XIcon,
       },
     ],
   },
   education: [
     {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
+      school: "Charles Sturt University",
+      degree: "Master of Networking and Systems Administration",
+      start: "2012",
+      end: "2012",
+    },
+    {
+      school: "Charles Sturt University",
+      degree: "Graduate Certificate in Information Systems Security",
+      start: "2011",
+      end: "2012",
     },
   ],
   work: [
     {
-      company: "Film.io",
-      link: "https://film.io",
-      badges: ["Remote", "React", "Next.js", "TypeScript", "Node.js"],
-      title: "Software Architect",
-      logo: ConsultlyLogo,
+      company: "TikTok USDS",
+      link: "https://usds.tiktok.com",
+      badges: ["Cloud", "Networking", "SRE", "Load Balancer", "Python"],
+      title: "Site Reliablility Engineer, Cloud Infrastructure",
       start: "2024",
       end: null,
       description: (
         <>
-          Leading technical architecture of a blockchain-based film funding
-          platform.
+          Build, design and maintain production cloud infrastructure.
+
           <ul className="list-inside list-disc">
             <li>
-              Architecting migration from CRA to Next.js for improved
-              performance, SEO, and DX
+              Build, maintain, and optimize cloud automation solutions using Ansible, Terraform,
+              cloud-init, and Python, reducing deployment times and increasing infrastructure consistency
             </li>
             <li>
-              Established release process enabling faster deployments and
-              reliable rollbacks
+              Design, provision, and manage server infrastructure across multi-cloud and hybrid environments,
+              ensuring scalability and reliability
             </li>
             <li>
-              Implementing system-wide monitoring and security improvements
+              Implement advanced load balancers with layer 3/layer 4 and layer 7 load balancers
+            </li>
+            <li>
+              Create comprehensive monitoring and observability solutions using cloud-native tooling,
+              Grafana, and Kibana, enabling proactive issue detection and rapid incident response
+            </li>
+            <li>
+              Perform in-depth troubleshooting and root cause analysis across complex bare-metal
+              and virtualized cloud infrastructure, minimizing service disruptions
             </li>
           </ul>
         </>
       ),
     },
     {
-      company: "Parabol",
-      link: "https://parabol.co",
+      company: "Endsight",
+      link: "https://endsight.net",
       badges: [
         "Remote",
-        "React",
-        "TypeScript",
-        "Node.js",
-        "GraphQL",
-        "Tailwind CSS",
+        "Python",
+        "Flask",
+        "Bootstrap",
+        "Operations",
       ],
-      title: "Senior Full Stack Developer",
-      logo: ParabolLogo,
-      start: "2021",
+      title: "Senior System Engineer",
+      start: "2023",
       end: "2024",
       description: (
         <>
-          Senior developer and squad leader for an enterprise agile meeting
-          platform.
+          As a Senior System Engineer at Endsight, I blend my DevOps and Site Reliability/Production Engineering
+          expertise with a diverse technology skill set to drive impactful business changes.
           <ul className="list-inside list-disc">
             <li>
-              Built design system with Tailwind CSS, improving development speed
-              and time to market
+              Spearheaded the design, and deployment of a company-wide monorepo using Git, Gitlab and Github,
+              enhancing code management and collaboration efficiency.
             </li>
             <li>
-              Implemented engineering practices: PR automation, code review
-              guidelines, and workflows
+              Developed comprehensive dashboards for aggregating multiple monitoring systems,
+              streamlining the process of data visualization and analytics.
+              This initiative significantly improved operational insights and decision-making capabilities.
             </li>
             <li>
-              Open source contributions to Relay DevTools and React i18n tooling
+              Engineered internal reporting tools for the Network Operations Center (NOC),
+              facilitating real-time data tracking and system performance analysis.
+            </li>
+            <li>
+              Conceptualized and deployed a web-based service catalog utilizing a blend of technologies including
+              HTML, Python, Flask, JavaScript, and Bootstrap.
+            </li>
+            <li>
+              At Endsight in my role as a Senior System Engineer I have a keen focus on automating processes,
+              optimizing system reliability, and implementing robust, scalable solutions that cater to the
+              dynamic needs of the business. As a Senior System Engineer, I not only focus on technical
+              implementation but also work to skill up the team through teaching and mentoring.
             </li>
           </ul>
         </>
       ),
     },
     {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
-      badges: ["Remote", "React", "TypeScript", "Node.js", "Android", "Kotlin"],
-      title: "Lead Android Developer → Full Stack Developer",
-      logo: ClevertechLogo,
-      start: "2015",
-      end: "2021",
+      company: "Facebook/Meta",
+      link: "https://facebook.com",
+      badges: ["Networking", "Cisco", "Juniper", "Arista", "Python"],
+      title: "Network Engineer → Production Engineer",
+      start: "2017",
+      end: "2023",
       description: (
         <>
-          Successfully transitioned from mobile to full-stack development while
-          leading distributed teams.
+          Production Engineer with a network specialization on the Infrastructure Network Engineering team.
+          The team is responsible for multiple different production networks at Meta.
+
+          Network engineer for the corporate networking team at Meta. Focused on the office network specifically.
+          Responsible for the wired and WAN edge network for the global office footprint and operationally
+          involved in the wireless and remote access VPN systems.
           <ul className="list-inside list-disc">
             <li>
-              Led frontend team at Evercast, building real-time platform
-              supporting 30+ users per room with HD streaming and collaboration
-              tools
+              Design new networks from requirements provided by customers and partners
             </li>
             <li>
-              Developed offline-first Android app for DKMS, improving donor
-              registration process
+              Operate a globally distributed office network with well over 100 locations and 10s of thousands of users
             </li>
             <li>
-              Led development teams across multiple successful client projects
+              Operate multiple distinct networks with 10s of thousands of devices
+            </li>
+            <li>
+              Build and maintain automation systems (primarily in Python with some Golang and C++/Rust based systems)
+            </li>
+            <li>
+              Build and action alerts and automate remediations
+            </li>
+            <li>
+              Build dashboards and SNMP/Netconf/XML/JSON monitoring of network device status
+            </li>
+            <li>
+              Member of a 24/7 oncall
+            </li>
+            <li>
+              Mentor and onboard new hires
+            </li>
+            <li>
+              Interview candidates in networking, coding and systems design (interviewed over 120 candidates)
+            </li>
+            <li>
+              Create and assist with the creation of team roadmaps along with project management of roadmap items
             </li>
           </ul>
         </>
       ),
-    },
-    {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
-      badges: ["On Site", "Android", "Java", "Kotlin"],
-      title: "Android Developer → Lead Android Developer",
-      logo: JojoMobileLogo,
-      start: "2012",
-      end: "2015",
-      description: (
-        <>
-          First Android developer, grew and led a team of 15+ engineers while
-          establishing engineering culture.
-          <ul className="list-inside list-disc">
-            <li>
-              Developed apps for major Polish companies including LOT, Polskie
-              Radio, and Agora
-            </li>
-            <li>Built and mentored high-performing mobile development team</li>
-          </ul>
-        </>
-      ),
-    },
-    {
-      company: "Nokia Siemens Networks",
-      link: "https://www.nokia.com",
-      badges: ["On Site", "C/C++", "LTE", "Agile"],
-      title: "C/C++ Developer",
-      logo: NSNLogo,
-      start: "2010",
-      end: "2012",
-      description:
-        "Developed software for LTE base stations at enterprise scale, gaining strong fundamentals in software architecture, testing practices, and cross-team collaboration.",
     },
   ],
   skills: [
-    "React/Next.js/Remix",
-    "TypeScript",
-    "Tailwind CSS",
-    "Design Systems",
-    "WebRTC",
-    "WebSockets",
-    "Node.js",
-    "GraphQL",
-    "Relay",
-    "System Architecture",
-    "Remote Team Leadership",
+    "Cisco",
+    "Arista",
+    "Juniper",
+    "Load Balancing",
+    "Python",
+    "Automation",
+    "DNS",
+    "DHCP",
+    "Troubleshooting",
+    "Wireshark",
+    "Cloud",
+    "Ansible",
+    "Terraform",
   ],
-  projects: [
-    {
-      title: "Monito",
-      techStack: ["TypeScript", "Next.js", "Browser Extension", "PostgreSQL"],
-      description:
-        "Browser extension for debugging web applications. Includes taking screenshots, screen recording, E2E tests generation and generating bug reports",
-      logo: MonitoLogo,
-      link: {
-        label: "monito.dev",
-        href: "https://monito.dev/",
-      },
-    },
-    {
-      title: "Consultly",
-      techStack: [
-        "TypeScript",
-        "Next.js",
-        "Vite",
-        "GraphQL",
-        "WebRTC",
-        "Tailwind CSS",
-        "PostgreSQL",
-        "Redis",
-      ],
-      description:
-        "Platform for online consultations with real-time video meetings and scheduling",
-      logo: ConsultlyLogo,
-      link: {
-        label: "consultly.com",
-        href: "https://consultly.com/",
-      },
-    },
-    {
-      title: "Minimalist CV",
-      techStack: ["TypeScript", "Next.js", "Tailwind CSS"],
-      description:
-        "An open source minimalist, print friendly CV template with a focus on readability and clean design. >9k stars on GitHub",
-      logo: MonitoLogo,
-      link: {
-        label: "Minimalist CV",
-        href: "https://github.com/BartoszJarocki/cv",
-      },
-    },
-  ],
+  projects: [],
 } as const;
